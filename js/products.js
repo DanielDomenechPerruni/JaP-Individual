310bd27ef4998650bc2e24c6bd26b5ba74bdfaad
danieldomenechperruni <danieldomenechperruni@gmail.com>
2022-08-10 23:25:03 -0300

    se agrega products

---
+++ js/products.js
@@ -0,0 +1,80 @@
+const LIST_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+
+function hideSpinner(){
+    document.getElementById("spinner-wrapper").style.display = "none";
+  }
+
+
+  function showSpinner(){
+    document.getElementById("spinner-wrapper").style.display = "block"; 
+  }
+
+
+let categoriesArray = [];
+
+
+function showCategoriesList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let products = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ products.name +`</h4> 
+                        <p> `+ products.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + products.soldCount + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+
+function getJSONData(url){
+    let result = {};
+    showSpinner(); 
+    return fetch(url) 
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+          hideSpinner(); 
+          return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
+        hideSpinner(); 
+        return result;
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(LIST_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            showCategoriesList(categoriesArray);
+        }
+    });
+});
