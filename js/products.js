27196ba55137acf0d17256a326ccbecc0cc4ebe6
danieldomenechperruni <danieldomenechperruni@gmail.com>
2022-08-30 22:12:45 -0300

    Filtros ascendentes y descendentes por precio listos. Filtro por relevancia pronto.

--- js/products.js
+++ js/products.js
@@ -1,6 +1,43 @@
 let item = localStorage.getItem("catID");
 const URLProductos = `https://japceibal.github.io/emercado-api/cats_products/${item}.json`;
 
+const ORDER_ASC_BY_PRICE = "$UP";
+const ORDER_DESC_BY_PRICE = "$DOWN";
+const ORDER_BY_PROD_REL = "Rel.";
+let productsArray = [];
+let currentSortCriteria = undefined;
+let minCount = undefined;
+let maxCount = undefined;
+
+function sortProductos(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_PRICE)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.cost < b.cost ){ return -1; }
+            if ( a.cost > b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_PRICE){
+        result = array.sort(function(a, b) {
+            if ( a.cost > b.cost ){ return -1; }
+            if ( a.cost < b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_BY_PROD_REL){
+        result = array.sort(function(a, b) {
+            let aSoldCount = parseInt(a.soldCount);
+            let bSoldCount = parseInt(b.soldCount);
+
+            if ( aSoldCount > bSoldCount ){ return -1; }
+            if ( aSoldCount < bSoldCount ){ return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+}
+
 function insertarProductos(productos) {
     let html = "";
     productos.forEach(producto => {
@@ -26,11 +63,42 @@ function insertarProductos(productos) {
     document.getElementById("lista-de-productos").innerHTML = html;
 }
 
+function sortAndShowProducts(sortCriteria, productsArray){
+    //let currentSortCriteria = sortCriteria;
+    //let currentProductsArray = [];
+    //console.log(productsArray);
+    if(productsArray != undefined){
+        //currentProductsArray = productsArray;
+        let currentProductsArray = sortProductos(sortCriteria, productsArray);
+        insertarProductos(currentProductsArray);
+    }
+
+    //currentProductsArray = sortProductos(currentSortCriteria, currentProductsArray);
+
+    //Muestro las categorías ordenadas
+    //insertarProductos(currentProductsArray);
+}
+
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(URLProductos).then(function(resultObj){
+        
         if (resultObj.status === "ok")
         {
-            insertarProductos(resultObj.data.products)
+            productsArray = resultObj.data.products;
+            insertarProductos(productsArray);
         }
     });
+
+    document.getElementById("sortPriceAsc").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_ASC_BY_PRICE, productsArray);
+    });
+
+    document.getElementById("sortPriceDesc").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_DESC_BY_PRICE, productsArray);
+    });
+
+    document.getElementById("sortByRelevance").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_BY_PROD_REL, productsArray);
+    });
+    
 });
