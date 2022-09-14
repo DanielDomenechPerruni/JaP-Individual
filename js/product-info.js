90a938c7ad968b5097e14a001efabcdf28240d86
danieldomenechperruni <danieldomenechperruni@gmail.com>
2022-09-13 22:06:32 -0300

    Pronta la seccion de comentarios y la base para enviarlos

--- js/product-info.js
+++ js/product-info.js
@@ -1,16 +1,56 @@
 let item = localStorage.getItem("productId");
 const URLProducto = `https://japceibal.github.io/emercado-api/products/${item}.json`;
+const URLComentarios = `https://japceibal.github.io/emercado-api/products_comments/${item}.json`;
 let producto = {};
+let comentario = [];
 
 function insertarImagenes () {
     html = "";
     producto.images.forEach(element => {
         html += `
-        <div><img src="${element}" alt=""></div>
+        <div><img src="${element}" alt="product image" class="img-thumbnail"></div>
         `;
     });
 
     document.getElementById("imagenesIlustrativas").innerHTML = html;
+}
+
+function insertarEstrellas (estrellas){
+    let puntuacion = "";
+    let contador = 0;
+    for (let index = 0; index < estrellas; index++) {
+        puntuacion += '<span class="fa fa-star checked"></span>';
+        contador += 1;
+    }
+    if (contador < 5) {
+        for (let index = contador; index < 5; index++) {
+            puntuacion += '<span class="fa fa-star"></span>';
+        }
+    }
+    
+    return puntuacion;
+}
+
+function insertarComentarios () {
+    html = "";
+    comentario.forEach(element => {
+        html += `
+        <div class="list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-between">
+                <div class="d-flex w-50 justify-content-between">
+                    <h4>${element.user}</h4>
+                    <p>${element.dateTime}</p>
+                </div>
+                <div>
+                    ${insertarEstrellas(element.score)}
+                </div>
+            </div>
+            <p>${element.description}</p>
+        </div>
+        `
+    })
+
+    document.getElementById("comentarios").innerHTML = html;
 }
 
 function insertarDatosDelProducto () {
@@ -27,7 +67,13 @@ document.addEventListener("DOMContentLoaded", function (e) {
         if (resultObj.status === "ok") {
             producto = resultObj.data;
             insertarDatosDelProducto();
-    }});
+    }}).then(
+        getJSONData(URLComentarios).then(function (resultObj) {
 
+            if (resultObj.status === "ok") {
+                comentario = resultObj.data;
+                insertarComentarios();
+        }}));
+    
 });
 
